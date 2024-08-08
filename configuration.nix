c72ac3cd1bacf9ca78edbe1f5f3824793c15e564
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2024-08-09 00:33:56 +1000

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,154 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelParams = [ "ip=dhcp" ];
+  boot.initrd = {
+    # The ethernet driver: `readlink /sys/class/net/eno1/device/driver`
+    availableKernelModules = [ "e1000e" ];
+    systemd.users.root.shell = "/bin/cryptsetup-askpass";
+    network = {
+      enable = true;
+      ssh = {
+        enable = true;
+	port = 2222;
+	authorizedKeys = config.users.users.lucas.openssh.authorizedKeys.keys;
+	hostKeys = [
+	  "/etc/secrets/initrd/ssh_host_ed25519_key"
+	];
+      };
+    };
+  };
+
+  # boot.initrd = let interface = "wlp0s20f0u6"; in
+  #   {
+  #     availableKernelModules = [ "ccm" "ctr" "iwlmvm" "iwlwifi" ];
+
+  #     systemd = {
+  #       enable = true;
+
+  # 	packages = [ pkgs.wpa_supplicant ];
+  # 	initrdBin = [ pkgs.wpa_supplicant ];
+  # 	targets.initrd.wants = [ "wpa_supplicant@${interface}.service" ];
+
+  # 	services."wpa_supplicant@".unitConfig.DefaultDependencies = false;
+
+  # 	users.root.shell = "/bin/systemd-tty-ask-password-agent";
+
+  # 	network.enable = true;
+  # 	network.networks."10-wlan" = {
+  # 	  matchConfig.Name = interface;
+  # 	  networkConfig.DHCP = "yes";
+  # 	};
+  #     };
+
+  #     secrets."/etc/wpa_supplicant/wpa_supplicant-${interface}.conf" = /root/secrets/wpa_supplicant.conf;
+      
+  #     network = {
+  #       enable = true;
+  #     	ssh = {
+  #         enable = true;
+  #         port = 22;
+  #         authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxfASmrb46pekGht2eINx1+ZsJwbvNm0EE51a1nXOu lucas_soriano@fastmail.com" ];
+  #         hostKeys = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  #     };
+  #   };
+  # };
+
+  console.useXkbConfig = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.nameservers = ["192.168.1.104"];
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Australia/Sydney";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "altgr-intl";
+    xkb.options = "ctrl:swapcaps";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.lucas = {
+    isNormalUser = true;
+    description = "Lucas";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+    emacs
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+  };
+
+  users.users."lucas".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxfASmrb46pekfGht2eINx1+ZsJwbvNm0EE51a1nXOu lucas_soriano@fastmail.com"
+  ];
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "nixlab";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
